5f1d93af95912548b144be77bb883408c7e7e90b
Sneha-Singh2 <snehasingh19050@gmail.com>
2025-06-12 02:23:04 +0530

    complete profile page UI - a little update

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import {useSelector} from 'react-redux';
 export default function Profile() {
   const {currentUser} = useSelector((state) => state.user)
   return (
-    <div className='p-3 max-w-lg mx-auto'>
+    <div className='p-3 max-w-lg mx-auto mt-12 rounded-xl shadow-md bg-amber-50'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
          <img src={currentUser.avatar} alt="profile"
